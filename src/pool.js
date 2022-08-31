6c1e55321f3ccb165a0b2eea1d80bbb9d3cd87d6
Citrom <citronejoe0123@gmail.com>
2022-08-31 13:45:49 +0200

    connection pool.js added

---
+++ src/pool.js
@@ -0,0 +1,27 @@
+const pg = require ('pg');
+
+// NORMALLY , create a pool like this: BUT this approach doesn't work well multiple DB
+
+/*
+const pool = new pg.Pool({
+    host: 'localhost',
+    port: 5432,
+    database: 'socialnetwork2nodeAPI',
+    user: 'servermyusername',
+    password: 'serverMyPassword'
+  });
+
+ module.exports = pool;
+  
+ */
+
+ class Pool {
+    _pool = null;
+
+    connect(options) {
+        this._pool = new pg.Pool(options)
+    }
+ }
+
+ module.exports = new Pool();
+ 
