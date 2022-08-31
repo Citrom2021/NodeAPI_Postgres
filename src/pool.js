00fef9bc63d25ac2feccbe961da4bec9c46ed861
Citrom <citronejoe0123@gmail.com>
2022-08-31 14:08:45 +0200

    conn close and query sql added

--- src/pool.js
+++ src/pool.js
@@ -15,12 +15,23 @@ const pool = new pg.Pool({
   
  */
 
+ // this approach via class supporting multiple DB conn
+ 
  class Pool {
     _pool = null;
 
     connect(options) {
         this._pool = new pg.Pool(options);
         return this._pool.query('SELECT 1 + 1');
+    }
+
+    close() {
+        return this._pool.end();
+    }
+
+    // BIG SECURITY ISSUE HERE TO FIX!
+    query(sql) {
+        return this._pool.query(sql);
     }
  }
 
